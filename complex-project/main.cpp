18bc847f655f2cd762f06583177643bd291a5f78
Syed Nouman Ali Shah <33029215+noumanalisyed@users.noreply.github.com>
2024-10-25 13:29:37 +0500

    Rational No Compilation issues resolved

--- complex-project/main.cpp
+++ complex-project/main.cpp
@@ -1,50 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class Complex{
-public:
-    Complex(){
-        realPart = 0;
-        imgPart = 0;
-        cout<<"Complex -- default constructor called "<<endl;
-    }
-    Complex(double realPart, double imgPart){
-        this->realPart = realPart;
-        Complex::imgPart = imgPart;
-        cout<<"Complex -- Overloaded constructor called "<<endl;
-    }
-    void display(){
-        cout<<"[ "<<realPart<<" + "<<imgPart<<"i ]"<<endl;
-    }
+#include "ComplexNo.h"
 
-    double getRealPart() {
-        return realPart;
-    }
-
-    void setRealPart(double r) {
-        realPart = r;
-    }
-
-    double getImgPart() {
-        return imgPart;
-    }
-    void setImgPart(double i) {
-        imgPart = i;
-    }
-    Complex add(Complex c){
-        double r = realPart + c.realPart;
-        double i = imgPart + c.imgPart;
-        return Complex(r,i);
-    }
-private:
-    double realPart, imgPart;
-};
 int main() {
-    Complex c1(7,8);
-    Complex c2(4,5);
+    ComplexNo c1(7,8);
+    ComplexNo c2(4,5);
     c1.display();
     c2.display();
-    Complex c3 = c1.add(c2);
+    ComplexNo c3 = c1.add(c2);
     c3.display();
+    ComplexNo c4 = c1.sub(c2);
+    c4.display();
     return 0;
 }
