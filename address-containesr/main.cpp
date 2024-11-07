c811379e78205bd48f0e1944e8eb692fbfd19a56
Syed Nouman Ali Shah <33029215+noumanalisyed@users.noreply.github.com>
2024-11-07 14:20:09 +0500

    two dimensional pointers processing

--- address-containesr/main.cpp
+++ address-containesr/main.cpp
@@ -32,6 +32,12 @@ int main() {
         dblPtr[i] = new int[3];
     }
 
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 3; j++) {
+            cout << "Enter Value at X[ " << i << "][ " << j << " ] = ";
+            cin >> dblPtr[i][j];
+        }
+    }
 
 
     return 0;
